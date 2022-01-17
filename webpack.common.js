21156b931cd67e1d6a1638ce51c4208d961dfc2d
Denis Nikitin <dinya.nikitin@gmail.com>
2022-01-17 15:28:03 +0300

    add url-loader

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,19 @@ module.exports = {
         test: /\.svg$/,
         type: 'asset/resource',
       },
+      {
+        test: /\.(png|jpg|gif)$/i,
+       
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
